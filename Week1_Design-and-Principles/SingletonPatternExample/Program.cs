8b14b12c804a777c08bcf3c62c7a726cb312e8ee
DEEPJYOTIROY2005 <22051421@kiit.ac.in>
2025-06-20 10:50:16 +0530

    Add files via upload

---
+++ Week1_Design-and-Principles/SingletonPatternExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            logger1.Log("Hello from logger1!");
+            logger2.Log("Hello from logger2!");
+
+            if (logger1 == logger2)
+            {
+                Console.WriteLine("Both logger1 and logger2 are the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Different instances exist!");
+            }
+
+            Console.ReadLine(); // Optional, keeps console open
+        }
+    }
+}
